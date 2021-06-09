2afa1a0fba4ab3b3e93ba6f611fce5803566085e
Dylan DesRosier <dylan@pooltogether.com>
2021-06-09 18:39:42 -0400

    fix border for banner and card

--- lib/components/Containers/Card.jsx
+++ lib/components/Containers/Card.jsx
@@ -2,16 +2,25 @@ import React from 'react'
 import classnames from 'classnames'
 
 export const Card = (props) => {
-  const { children, className } = props
+  const { children, className, paddingClassName, sizeClassName, backgroundClassName } = props
 
   return (
     <div
       className={classnames(
-        'bg-default py-3 px-3 xs:py-6 xs:px-8 sm:py-6 sm:px-12 rounded-xl w-full fadeIn animated',
+        'rounded-xl fadeIn animated',
+        sizeClassName,
+        paddingClassName,
+        backgroundClassName,
         className
       )}
     >
       {children}
     </div>
   )
+}
+
+Card.defaultProps = {
+  paddingClassName: 'p-4 xs:py-6 xs:px-8 sm:py-6 sm:px-12',
+  sizeClassName: 'w-full',
+  backgroundClassName: 'bg-default'
 }
