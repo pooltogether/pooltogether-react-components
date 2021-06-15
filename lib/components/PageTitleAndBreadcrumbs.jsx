8879d1325ab7022999c3338594e698b44bf0ea65
Dylan DesRosier <dylan@pooltogether.com>
2021-06-15 10:54:38 -0400

    add className for breadcrumbs

--- lib/components/PageTitleAndBreadcrumbs.jsx
+++ lib/components/PageTitleAndBreadcrumbs.jsx
@@ -1,51 +1,55 @@
 import React from 'react'
+import classnames from 'classnames'
 
 import { PoolCurrencyIcon } from 'lib/components/PoolCurrencyIcon'
 import { Chip } from 'lib/components/Containers/Chip'
 
 // TODO: split into two components: 'PageTitle' and a separate 'Breadcrumbs'
 export const PageTitleAndBreadcrumbs = (props) => {
-  const { Link, t, breadcrumbs, title, pool } = props
+  const { Link, t, breadcrumbs, title, pool, className } = props
 
   const crumbJsx = (
-    <>
-      <div className='flex flex-col items-start justify-between w-full leading-none'>
-        <div className='inline-block text-left text-xl sm:text-3xl font-bold text-accent-2 relative'>
-          {title}
-        </div>
-        <div
-          className='inline-block text-left text-caption-2 relative uppercase mt-3'
-          style={{
-            left: 1,
-            bottom: 2
-          }}
-        >
-          {breadcrumbs?.map((crumb, index) => (
-            <span key={`crumb-${index}`}>
-              {crumb.href && crumb.as ? (
-                <>
-                  <Link href={crumb.href} as={crumb.as} shallow>
-                    <a className='text-xxxs border-b border-accent-3'>{crumb.name}</a>
-                  </Link>
-                </>
-              ) : (
-                <>
-                  <span className='text-xxxs'>{crumb.name}</span>
-                </>
-              )}
-              {index + 1 !== breadcrumbs.length && <> &gt; </>}
-            </span>
-          ))}
-        </div>
+    <div
+      className={classnames(
+        'flex flex-col items-start justify-between w-full leading-none',
+        className
+      )}
+    >
+      <div className='inline-block text-left text-xl sm:text-3xl font-bold text-accent-2 relative'>
+        {title}
       </div>
-    </>
+      <div
+        className='inline-block text-left text-caption-2 relative uppercase mt-3'
+        style={{
+          left: 1,
+          bottom: 2
+        }}
+      >
+        {breadcrumbs?.map((crumb, index) => (
+          <span key={`crumb-${index}`}>
+            {crumb.href && crumb.as ? (
+              <>
+                <Link href={crumb.href} as={crumb.as} shallow>
+                  <a className='text-xxxs border-b border-accent-3'>{crumb.name}</a>
+                </Link>
+              </>
+            ) : (
+              <>
+                <span className='text-xxxs'>{crumb.name}</span>
+              </>
+            )}
+            {index + 1 !== breadcrumbs.length && <> &gt; </>}
+          </span>
+        ))}
+      </div>
+    </div>
   )
 
   return (
     <>
       {pool ? (
         <>
-          <div className='flex justify-start items-center'>
+          <div className={classnames('flex justify-start items-center', className)}>
             <PoolCurrencyIcon xl pool={pool} />
 
             <div className='ml-1 sm:ml-6'>{crumbJsx}</div>
