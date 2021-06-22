19ae7192dddf0553b4820793dfe5dcd45868a97f
Dylan DesRosier <dylan@pooltogether.com>
2021-06-22 15:04:11 -0400

    fix bottom nav color

--- lib/components/Navigation/BottomNavContainer.jsx
+++ lib/components/Navigation/BottomNavContainer.jsx
@@ -7,7 +7,7 @@ import React from 'react'
  */
 export const BottomNavContainer = (props) => (
   <nav
-    className='w-screen flex justify-center items-center b-0 l-0 r-0 bg-card sm:hidden z-20 pb-6'
+    className='w-screen flex justify-center items-center b-0 l-0 r-0 bg-card-selected sm:hidden z-20 pb-6'
     style={{
       height: 96
     }}
