aa927eafaca356c76f5fe970114c166c88827453
Charles Bergeron <chuck.bergeron@gmail.com>
2021-07-19 12:34:54 -0700

    * remove uppercased image filenames

--- lib/components/Icons/TokenIcon.jsx
+++ lib/components/Icons/TokenIcon.jsx
@@ -8,7 +8,7 @@ export const TokenIcon = (props) => {
 
   const { data: tokenImage, isFetched } = useCoingeckoTokenImage(chainId, address)
 
-  const imageOverride = TOKEN_IMAGE_OVERRIDES?.[chainId]?.[address]
+  const imageOverride = TOKEN_IMAGE_OVERRIDES?.[chainId]?.[address.toLowerCase()]
 
   if (imageOverride || isFetched) {
     const src = imageOverride || tokenImage
@@ -52,8 +52,6 @@ import USDC from '../../assets/Tokens/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48
 import UNI from '../../assets/Tokens/0x1f9840a85d5af5bf1d1762f925bdaddc4201f984.png'
 import DPI from '../../assets/Tokens/0x1494ca1f11d487c2bbe4543e90080aeba4ba3c2b.png'
 import USDT from '../../assets/Tokens/0xc2132d05d31c914a87c6611c10748aeb04b58e8f.png'
-import PCDAI from '../../assets/Tokens/0x0a2e7f69fe9588fa7fba5f5864236883cd4aac6d.png'
-import PCUSDC from '../../assets/Tokens/0x391a437196c81eea7bbbbd5ed4df6b49de4f5c96.png'
 import SUSHI from '../../assets/Tokens/0x6b3595068778dd592e39a122f4f5a5cf09c90fe2.png'
 import COMP from '../../assets/Tokens/0xc00e94cb662c3520282e6f5717214004a7f26888.svg'
 import BOND from '../../assets/Tokens/0x0391d2021f89dc339f60fff84546ea23e337750f.png'
@@ -61,16 +59,18 @@ import AAVE from '../../assets/Tokens/0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9
 import DEFISOCKS from '../../assets/Tokens/0x9d942bd31169ed25a1ca78c776dab92de104e50e.png'
 import BOOKS from '../../assets/Tokens/0x117c2aca45d87958ba054cb85af0fd57be00d624.png'
 import LOTTO from '../../assets/Tokens/0xb0dfd28d3cf7a5897c694904ace292539242f858.png'
-import PPOOL from '../../assets/Tokens/0x27D22A7648e955E510a40bDb058333E9190d12D4.png'
-import PCcCOMP from '../../assets/Tokens/0x27B85f596feB14e4B5fAA9671720a556a7608C69.png'
-import PcUSDC from '../../assets/Tokens/0xD81b1A8B1AD00Baa2D6609E0BAE28A38713872f7.png'
-import PcUNI from '../../assets/Tokens/0xA92a861FC11b99b24296aF880011B47F9cAFb5ab.png'
-import PcDAI from '../../assets/Tokens/0x334cBb5858417Aee161B53Ee0D5349cCF54514CF.png'
-import PTXSushiTicket from '../../assets/Tokens/0xfdC192C153044dEdB67c5A17B8651951Cf70ee4a.png'
-import PTBadgerTicket from '../../assets/Tokens/0xfa831A04Cb52fc89Dd519D08dC5e94ab2dF52B7E.png'
-import PTUSDTSponsorship from '../../assets/Tokens/0x85e16156eb86a134ac6db5754be6C5E1C7f1Aa59.png'
-import PTUSDTTicket from '../../assets/Tokens/0x9ecB26631098973834925eb453De1908Ea4bdD4e.png'
 
+import PPOOL from '../../assets/Tokens/0x27d22a7648e955e510a40bdb058333e9190d12d4.png'
+import PCCCOMP from '../../assets/Tokens/0x27b85f596feb14e4b5faa9671720a556a7608c69.png'
+import PCUSDC from '../../assets/Tokens/0xd81b1a8b1ad00baa2d6609e0bae28a38713872f7.png'
+import PCUNI from '../../assets/Tokens/0xa92a861fc11b99b24296af880011b47f9cafb5ab.png'
+import PCDAI from '../../assets/Tokens/0x334cbb5858417aee161b53ee0d5349ccf54514cf.png'
+import PT_DAI_SPONSORSHIP from '../../assets/Tokens/0x0a2e7f69fe9588fa7fba5f5864236883cd4aac6d.png'
+import PT_USDC_SPONSORSHIP from '../../assets/Tokens/0x391a437196c81eea7bbbbd5ed4df6b49de4f5c96.png'
+import PT_XSUSHI_TICKET from '../../assets/Tokens/0xfdc192c153044dedb67c5a17b8651951cf70ee4a.png'
+import PT_BADGER_TICKET from '../../assets/Tokens/0xfa831a04cb52fc89dd519d08dc5e94ab2df52b7e.png'
+import PT_USDT_SPONSORSHIP from '../../assets/Tokens/0x85e16156eb86a134ac6db5754be6c5e1c7f1aa59.png'
+import PT_USDT_TICKET from '../../assets/Tokens/0x9ecb26631098973834925eb453de1908ea4bdd4e.png'
 // Polygon
 import WMATIC from '../../assets/Tokens/0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270.png'
 
@@ -102,19 +102,17 @@ export const TOKEN_IMAGE_OVERRIDES = Object.freeze({
     '0x1494ca1f11d487c2bbe4543e90080aeba4ba3c2b': DPI,
     '0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0': WMATIC,
     '0xdac17f958d2ee523a2206206994597c13d831ec7': USDT,
-    '0x0a2e7f69fe9588fa7fba5f5864236883cd4aac6d': PCDAI,
     '0x334cbb5858417aee161b53ee0d5349ccf54514cf': PCDAI,
-    '0x391a437196c81eea7bbbbd5ed4df6b49de4f5c96': PCUSDC,
     '0xd81b1a8b1ad00baa2d6609e0bae28a38713872f7': PCUSDC,
     '0x27D22A7648e955E510a40bDb058333E9190d12D4': PPOOL,
-    '0x27B85f596feB14e4B5fAA9671720a556a7608C69': PCcCOMP,
-    '0xD81b1A8B1AD00Baa2D6609E0BAE28A38713872f7': PcUSDC,
-    '0xA92a861FC11b99b24296aF880011B47F9cAFb5ab': PcUNI,
-    '0x334cBb5858417Aee161B53Ee0D5349cCF54514CF': PcDAI,
-    '0xfdC192C153044dEdB67c5A17B8651951Cf70ee4a': PTXSushiTicket,
-    '0xfa831A04Cb52fc89Dd519D08dC5e94ab2dF52B7E': PTBadgerTicket,
-    '0x85e16156eb86a134ac6db5754be6C5E1C7f1Aa59': PTUSDTSponsorship,
-    '0x9ecB26631098973834925eb453De1908Ea4bdD4e': PTUSDTTicket
+    '0x27b85f596feb14e4b5faa9671720a556a7608c69': PCCCOMP,
+    '0xa92a861fc11b99b24296af880011b47f9cafb5ab': PCUNI,
+    '0xfa831a04cb52fc89dd519d08dc5e94ab2df52b7e': PT_BADGER_TICKET,
+    '0x0a2e7f69fe9588fa7fba5f5864236883cd4aac6d': PT_DAI_SPONSORSHIP,
+    '0x391a437196c81eea7bbbbd5ed4df6b49de4f5c96': PT_USDC_SPONSORSHIP,
+    '0x85e16156eb86a134ac6db5754be6c5e1c7f1aa59': PT_USDT_SPONSORSHIP,
+    '0x9ecb26631098973834925eb453de1908ea4bdd4e': PT_USDT_TICKET,
+    '0xfdc192c153044dedb67c5a17b8651951cf70ee4a': PT_XSUSHI_TICKET
   },
   [NETWORK.polygon]: {
     '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270': WMATIC,
