8a85d94d288fda71124d679422b972e66e449de2
Dylan DesRosier <dylan@pooltogether.com>
2021-08-28 17:27:34 -0400

    wip

---
+++ lib/components/Modal/Modal.tsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import classnames from 'classnames'
+import FeatherIcon from 'feather-icons-react'
+import Dialog from '@reach/dialog'
+import { motion } from 'framer-motion'
+import { useReducedMotion } from '@pooltogether/hooks'
+
+export interface ModalProps {
+  isOpen: boolean
+  closeModal: () => void
+  children: React.ReactNode
+  label: string
+  className?: string
+  noPad?: boolean
+  noSize?: boolean
+  noBgColor?: boolean
+  noTextColor?: boolean
+  noRoundCorners?: boolean
+}
+
+export const Modal = (props: ModalProps) => {
+  const {
+    isOpen,
+    closeModal,
+    children,
+    label,
+    className,
+    noPad,
+    noSize,
+    noBgColor,
+    noTextColor,
+    noRoundCorners
+  } = props
+
+  const shouldReduceMotion = useReducedMotion()
+
+  if (!label) {
+    console.warn('Modal required a label! <Modal /> with children:', children)
+  }
+
+  return (
+    <Dialog aria-label={label} isOpen={isOpen} onDismiss={closeModal}>
+      <motion.div
+        id='modal-animation-wrapper'
+        key={label}
+        transition={{ duration: shouldReduceMotion ? 0 : 0.1, ease: 'easeIn' }}
+        initial={{
+          opacity: 0
+        }}
+        exit={{
+          opacity: 0
+        }}
+        animate={{
+          opacity: 1
+        }}
+        className={classnames(
+          'mx-auto relative',
+          {
+            'p-4': !noPad,
+            'bg-card': !noBgColor,
+            'text-inverse': !noTextColor,
+            'h-full sm:h-auto sm:max-w-sm': !noSize,
+            'rounded-none sm:rounded-xl ': !noRoundCorners
+          },
+          className
+        )}
+      >
+        <CloseModalButton closeModal={closeModal} />
+        {children}
+      </motion.div>
+    </Dialog>
+  )
+}
+
+Modal.defaultProps = {
+  noPad: false,
+  noSize: false
+}
+
+const CloseModalButton = (props) => {
+  const { closeModal } = props
+  return (
+    <button
+      className='my-auto ml-auto close-button trans text-inverse opacity-50 hover:opacity-100 absolute right-7 top-6'
+      onClick={closeModal}
+    >
+      <FeatherIcon icon='x' className='w-6 h-6' />
+    </button>
+  )
+}
---
+++ lib/components/Prizes/SimpleCountdown.tsx
@@ -0,0 +1,39 @@
+import { useTimeCountdown } from '@pooltogether/hooks'
+import React from 'react'
+
+interface SimpleCountDownProps {
+  seconds: number
+  t?: (key: string) => string
+}
+
+export const SimpleCountDown = (props: SimpleCountDownProps) => {
+  const { seconds: initialSecondsLeft, t } = props
+  const { days, hours, minutes, seconds } = useTimeCountdown(initialSecondsLeft)
+  const dayString = getTimeString(t, 'day', days)
+  const hoursString = getTimeString(t, 'hour', hours)
+  const minutesString = getTimeString(t, 'minute', minutes)
+  const secondsString = getTimeString(t, 'second', seconds)
+
+  if (days > 0) {
+    if (hours > 0) {
+      return <>{`${dayString} ${hoursString}`}</>
+    } else {
+      return <>{`${dayString} ${minutesString}`}</>
+    }
+  }
+
+  if (hours > 0) {
+    return <>{`${hoursString} ${minutesString}`}</>
+  }
+
+  if (minutes > 0) {
+    return <>{`${minutesString}`}</>
+  } else {
+    return <>{secondsString}</>
+  }
+}
+
+const getTimeString = (t, unit, amount) =>
+  `${amount} ${
+    t ? t(unit, { count: amount }) : amount > 0 ? `${unit}${amount === 1 ? '' : 's'}` : ''
+  }`
