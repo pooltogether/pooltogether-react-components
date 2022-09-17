19534ed404c1c49b4e5061de3c5a53500e811fda
Dylan DesRosier <dylan@pooltogether.com>
2022-09-17 01:07:39 -0400

    Fix loading logo

--- src/components/Loading/LoadingLogo.tsx
+++ src/components/Loading/LoadingLogo.tsx
@@ -8,12 +8,12 @@ import LogoDark from '../../assets/PoolTogetherLogos/pooltogether-p.svg'
 import Logo from '../../assets/PoolTogetherLogos/pooltogether-p-purple.svg'
 
 export function LoadingLogo(props) {
-  const { theme } = useTheme()
+  const { theme, systemTheme } = useTheme()
 
   return (
     <div className={classnames('flex flex-col', props.className)} style={{ width: 'min-content' }}>
       <img
-        src={theme === 'dark' ? LogoDark : Logo}
+        src={theme === 'dark' || (theme === 'system' && systemTheme === 'dark') ? LogoDark : Logo}
         className='w-8 mx-auto'
         style={{ borderWidth: 0 }}
       />
